b41e05ee16c31cc9a002a7f98ced040be11883c0
申准 <zhun.shen@corp.elong.com>
2017-11-12 14:13:22 +0800

    aaa

--- JavaBasic/src/main/java/io/github/johnshen1990/javacodelab/javabasic/lintcode/Strstr.java
+++ JavaBasic/src/main/java/io/github/johnshen1990/javacodelab/javabasic/lintcode/Strstr.java
@@ -9,106 +9,47 @@ public class Strstr {
     public static void main(String[] args) {
         String source = "asdbd";
         String target = "bd";
-        int r = strStr3(source, target);
+        int r = strStr(source, target);
         System.out.println(r);
 
     }
 
     /**
-     * 调用了Java String的库方法(equals,substring),略有作弊嫌疑
+     * 朴素匹配法
      * @param source
      * @param target
      * @return
      */
-    public static int strStr1(String source, String target) {
-        int m = source.length();
-        int n = target.length();
-        if(m == 0 || n == 0) return -1;
-        if(m < n) return -1;
-        if(m == n) return source.equals(target)? 0 : -1;
-        for(int i=0; m-i >= n; i++){
-            String tmp = source.substring(i, i+n);
-            if(tmp.equals(target)) return i;
-        }
-        return -1;
-    }
-
-    /**
-     * 简单粗暴版
-     * source.length = M
-     * targe.length = N
-     * 最坏时间复杂度O((N-1)*(M-N))?
-     * @param source
-     * @param target
-     * @return
-     */
-    public static int strStr2(String source, String target) {
+    public static int strStr(String source, String target) {
         // 容错
-        if(source == null || target == null
-                || source.length() < target.length()) {
+        if(source == null || target == null) {
             return -1;
         }
-        // 语义上 空串包含空串 返0
-        if(source.length() == 0 && target.length() == 0) {
+        int n = source.length();
+        int m = target.length();
+
+        if(m == 0) {
             return 0;
         }
 
-        // start指向source的开始比较的下标
-        int start = 0;
-        while (source.length() - start >= target.length()) {
-            int j = start;
-            for(int i = 0; i < target.length(); i++,j++) {
-                if(target.charAt(i) != source.charAt(j)) {
+        if(n < m) {
+            return -1;
+        }
+
+        for(int i = 0; i <= n - m; i++) {
+            int j;
+            for(j = 0; j < m; j++) {
+                if(source.charAt(i + j) != target.charAt(j)) {
                     break;
                 }
             }
-            if(j < start + target.length()) {
-                start++;
-                continue;
-            }
-            return start;
-        }
-
-        return -1;
-    }
-
-    /**
-     * 使用Rabin-Karp算法
-     * @param source
-     * @param target
-     * @return
-     */
-    public static int strStr3(String source, String target) {
-        // 容错
-        if(source == null || target == null
-                || source.length() < target.length()) {
-            return -1;
-        }
-        // 语义上 空串包含空串 返0
-        if(source.length() == 0 && target.length() == 0) {
-            return 0;
-        }
-
-        // start指向source的开始比较的下标
-        int start = 0;
-        int targetHash = target.hashCode();
-        while (source.length() - start >= target.length()) {
-            String tmp = source.substring(start, start + target.length());
-            if(targetHash == tmp.hashCode()) {
-                int j = start;
-                for(int i = 0; i < target.length(); i++,j++) {
-                    if(target.charAt(i) != source.charAt(j)) {
-                        break;
-                    }
-                }
-                if(j < start + target.length()) {
-                    start++;
-                    continue;
-                }
-                return start;
+            if(j >= m) {
+                return i;
             }
         }
 
         return -1;
     }
+
+
 }
--- JavaBasic/src/main/java/io/github/johnshen1990/javacodelab/javabasic/lintcode/Strstr2.java
+++ JavaBasic/src/main/java/io/github/johnshen1990/javacodelab/javabasic/lintcode/Strstr2.java
@@ -7,35 +7,13 @@ package io.github.johnshen1990.javacodelab.javabasic.lintcode;
  */
 public class Strstr2 {
     public static void main(String[] args) {
-        String source = "asdbd";
-        String target = "bd";
+        String source = "abcde";
+        String target = "abc";
         int r = strStr2(source, target);
         System.out.println(r);
 
     }
 
-    /**
-     * 逐位比较两个字符串的值,判断是否equal
-     * 最坏时间复杂度O(m)
-     * @param a
-     * @param b
-     * @return
-     */
-    private static boolean isEqual(String a, String b) {
-        if(a == null || b == null) {
-            return false;
-        }
-        if(a.length() != b.length()) {
-            return false;
-        }
-
-        for(int i = 0; i < a.length(); i++) {
-            if(a.charAt(i) != b.charAt(i)) {
-                return false;
-            }
-        }
-        return true;
-    }
     /**
      * 使用Rabin-Karp算法
      * @param source
@@ -50,19 +28,60 @@ public class Strstr2 {
         int n = source.length();
         int m = target.length();
 
+        if(m == 0) {
+            return 0;
+        }
+
         if(n < m) {
             return -1;
         }
 
-        // 语义上 空串为任何串的子串 返0
-        if(target.length() == 0) {
-            return 0;
+
+        // BASE = 10^6
+        int BASE = 1000000;
+
+        // pow = 31^m
+        int pow = 1;
+        for(int i = 0; i < m; i++) {
+            pow = pow * 31 % BASE;
         }
 
-//        int targetHashcode = 0;
-//        for(int i = 0; i < m; i++) {
-//            targetHashcode * 31
-//        }
+        // targetCode = hash(target)
+        int targetCode = 0;
+        for(int i = 0; i < m; i++) {
+            targetCode = (targetCode * 31 + target.charAt(i)) % BASE;
+        }
+
+        // source = abcde
+        // target = cde
+
+        // hash(abc)
+        int hashCode = 0;
+        for(int i = 0; i < m; i++) {
+            hashCode = (hashCode * 31 + source.charAt(i)) % BASE;
+        }
+
+        for(int i = 0; i <= n - m; i++) {
+            if(hashCode == targetCode) {
+                String subStr = source.substring(i, i + m);
+                if(subStr.equals(target)) {
+                    return i;
+                }
+            }
+
+            if(i < n - m) {
+                // hash(abc) --> hash(abcd)
+                hashCode = (hashCode * 31 + source.charAt(i + m)) % BASE;
+
+                // hash(abcd) --> hash(bcd)
+                hashCode = hashCode - source.charAt(i) * pow % BASE;
+
+                if(hashCode < 0) {
+                    hashCode += BASE;
+                }
+            }
+
+        }
 
         return -1;
     }
